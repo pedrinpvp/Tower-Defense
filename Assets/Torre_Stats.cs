d8b780aa67e4ef40cd55b4ac922bfd20bafc1306
pedrinpvp <93227143+pedrinpvp@users.noreply.github.com>
2021-11-09 19:53:37 -0300

    torre

--- Assets/Torre_Stats.cs
+++ Assets/Torre_Stats.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Torre_Stats : MonoBehaviour
 {
+    public Torre_Scr torre;
+    public CircleCollider2D circle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        circle = gameObject.GetComponent<CircleCollider2D>();
+        circle.radius = torre.alcance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    
 }
