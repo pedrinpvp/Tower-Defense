5aca7fab2e789b45516c1a16b53e4e4d33256c9c
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2022-02-03 00:44:11 -0300

    Update Mob_Scr.cs

--- Assets/Mob_Scr.cs
+++ Assets/Mob_Scr.cs
@@ -9,10 +9,12 @@ public class Mob_Scr : ScriptableObject
     private string nome;
     public int danoAoCastelo = 3;
     public int vida = 10;
+    public int recompensa = 1;
     [Header("Quantos metros anda por segundo")]
     public float velocidade = 3;
     [Header("Imagem do inimigo")]
     public AnimatorController animation;
+
     private void OnValidate()
     {
         nome = name;
